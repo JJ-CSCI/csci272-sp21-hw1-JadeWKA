81be22c11b01cfbb08b4497c8472cdc99a19840d
Wing Ki Au <wingki.au@jjay.cuny.edu>
2021-02-23 02:43:11 +0000

    set up .h

---
+++ binomial.h
@@ -0,0 +1,34 @@
+#ifndef Binomial_h
+#define Binomial_h
+
+class Binomial{
+  private:
+  float coefficient[3]{0.0,1.0,1.0};
+  int power[3]{0,1,1};
+
+  public:
+  Binomial();
+
+  Binomial(float );
+
+  Binomial(float a, int x);
+
+  Binomial(float a, int x, float b);
+
+  Binomial(float a, int x, float b, int y);
+
+  int GetPower(int c);
+
+  float GetCoefficient(int m);
+
+  int SetPower(int d, int e);
+
+  int Add(Binomial o);
+
+  void Multiply(float h);
+
+  void Multiply(float h, int k);
+
+  };
+
+  #endif
